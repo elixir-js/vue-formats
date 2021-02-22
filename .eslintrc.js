d6393b2a53c61027f1a3ac1afb06bcdde60daddc
voidjmp <voidjmp@mail.ru>
2021-02-22 08:31:07 +0500

    :tada: Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        'jest/globals': true,
+    },
+    extends: ['airbnb-base'],
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['jest'],
+    rules: {
+        indent: ['error', 4],
+        'operator-linebreak': [2, 'after', { overrides: { '?': 'after', ':': 'after' } }],
+        linebreak: 0,
+    },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    printWidth: 100,
+    singleQuote: true,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    endOfLine: 'auto',
+    tabWidth: 4,
+    semi: true,
+};
